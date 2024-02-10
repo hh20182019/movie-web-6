9ed8092fffb0c5903b586ca98fd6d33c3ffa2d3e
hh20182019 <135841379+hh20182019@users.noreply.github.com>
2024-02-10 21:24:07 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,19 @@
+window.__CONFIG__ = {
+  // The URL for the CORS proxy, the URL must NOT end with a slash!
+  VITE_CORS_PROXY_URL: "https://beautiful-sundae-737021.netlify.app",
+
+  // The READ API key to access TMDB
+  VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0OGE4ZjM4OTk0YTQxOGE1Zjk3N2E5ZTE0Yjg1MTQ1ZCIsInN1YiI6IjY1YzQ3YjYyZjQ5NWVlMDE1NDBiZjNhOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.hrCiy6K7d9PCWvMF7tf_CgdtKhbmJekk5_ym8YG7bm0",
+
+  // The DMCA email displayed in the footer, null to hide the DMCA link
+  VITE_DMCA_EMAIL: null,
+
+  // Whether to disable hash-based routing, leave this as false if you don't know what this is
+  VITE_NORMAL_ROUTER: false,
+
+  // The backend URL to communicate with, defaults to the movie-web hosted one at backend.movie-web.app
+  VITE_BACKEND_URL: "http://www.hy464c.top:10881",
+
+  // A comma separated list of disallowed IDs in the case of a DMCA claim - in the format "series-<id>" and "movie-<id>"
+  VITE_DISALLOWED_IDS: ""
+};
